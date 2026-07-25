1d38c1b8e3331f145a2c6e2369f6972483a6db8e
Nishii <cookiejinmin@gmail.com>
2026-07-25 22:38:29 +0200

    Create authOptions.ts

---
+++ lib/authOptions.ts
@@ -0,0 +1,23 @@
+import type { NextAuthOptions } from "next-auth";
+import DiscordProvider from "next-auth/providers/discord";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID as string,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
+      authorization: { params: { scope: "identify" } },
+    }),
+  ],
+  callbacks: {
+    async session({ session, token }) {
+      if (session.user) {
+        (session.user as any).id = token.sub;
+      }
+      return session;
+    },
+  },
+  pages: {
+    // Cuando tengas diseño propio de login, apunta aquí, ej: "/login"
+  },
+};
